a3dfd091a6f1a65ebd0757984dc9a8d25f5943e0
asad <asadsyed924@gmail.com>
2021-08-10 13:32:04 +0500

    Add Name

--- Dashboard.py
+++ Dashboard.py
@@ -55,6 +55,7 @@ class Ui_MainWindow(object):
 "\n"
 "\n"
 "}")
+        # Asad
         self.UsernameLabel.setObjectName("UsernameLabel")
         self.MsgLabel = QtWidgets.QLabel(self.centralwidget)
         self.MsgLabel.setGeometry(QtCore.QRect(812, 27, 16, 20))
