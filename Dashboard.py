6091eca8e5e664ffa6bf799d4cdf606142548bde
NoorChishti <noorrchishti@gmail.com>
2021-08-10 12:44:34 -0700

    check hasnain

--- Dashboard.py
+++ Dashboard.py
@@ -7,6 +7,8 @@
 
 
 from PyQt6 import QtCore, QtGui, QtWidgets
+from PyQt6.QtCore import QSize
+from PyQt6.QtGui import QPixmap, QIcon
 
 
 class Ui_MainWindow(object):
@@ -17,16 +19,20 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         self.MainScreen = QtWidgets.QLabel(self.centralwidget)
         self.MainScreen.setGeometry(QtCore.QRect(0, 0, 1024, 768))
-        self.MainScreen.setStyleSheet("image: url(:/newPrefix/Dashboard.jpg);")
+        self.MainScreen.setPixmap(QPixmap("./Images/Dashboard.jpg"))
+        # self.MainScreen.setStyleSheet("image: url(:/newPrefix/Dashboard.jpg);")
         self.MainScreen.setText("")
         self.MainScreen.setObjectName("MainScreen")
         self.SideBarLabel = QtWidgets.QLabel(self.centralwidget)
         self.SideBarLabel.setGeometry(QtCore.QRect(-10, -15, 96, 795))
-        self.SideBarLabel.setStyleSheet("image: url(:/newPrefix/Side Bar.jpg);")
+        self.SideBarLabel.setPixmap(QPixmap("./Images/Side Bar.jpg"))
+        # self.SideBarLabel.setStyleSheet("image: url(:/newPrefix/Side Bar.jpg);")
         self.SideBarLabel.setText("")
         self.SideBarLabel.setObjectName("SideBarLabel")
         self.ProfilepushButton = QtWidgets.QPushButton(self.centralwidget)
         self.ProfilepushButton.setGeometry(QtCore.QRect(947, 15, 44, 44))
+        self.ProfilepushButton.setIconSize(QSize(40, 40))
+        self.ProfilepushButton.setIcon(QIcon("./Images/Path 206.png"))
         self.ProfilepushButton.setStyleSheet("#ProfilepushButton{\n"
 "image: url(:/newPrefix/Path 206.png);\n"
 "border: 1px solid #00A65A;\n"
@@ -52,12 +58,14 @@ class Ui_MainWindow(object):
         self.UsernameLabel.setObjectName("UsernameLabel")
         self.MsgLabel = QtWidgets.QLabel(self.centralwidget)
         self.MsgLabel.setGeometry(QtCore.QRect(812, 27, 16, 20))
-        self.MsgLabel.setStyleSheet("image: url(:/newPrefix/Icon feather-message-square.png);")
+        self.MsgLabel.setPixmap(QPixmap("./Images/Icon feather-message-square.png"))
+        # self.MsgLabel.setStyleSheet("image: url(:/newPrefix/Icon feather-message-square.png);")
         self.MsgLabel.setText("")
         self.MsgLabel.setObjectName("MsgLabel")
         self.NotiLabel = QtWidgets.QLabel(self.centralwidget)
         self.NotiLabel.setGeometry(QtCore.QRect(764, 27, 16, 20))
-        self.NotiLabel.setStyleSheet("image: url(:/newPrefix/Icon ionic-ios-notifications-outline.png);")
+        self.NotiLabel.setPixmap(QPixmap("./Images/Icon ionic-ios-notifications-outline.png"))
+        # self.NotiLabel.setStyleSheet("image: url(:/newPrefix/Icon ionic-ios-notifications-outline.png);")
         self.NotiLabel.setText("")
         self.NotiLabel.setObjectName("NotiLabel")
         self.DayandDate = QtWidgets.QLabel(self.centralwidget)
@@ -161,10 +169,13 @@ class Ui_MainWindow(object):
         self.NumLabel_2.setObjectName("NumLabel_2")
         self.GraphLabel = QtWidgets.QLabel(self.centralwidget)
         self.GraphLabel.setGeometry(QtCore.QRect(614, 498, 299, 130))
-        self.GraphLabel.setStyleSheet("image: url(:/newPrefix/Graph.png);")
+        self.GraphLabel.setPixmap(QPixmap("./Images/Graph.png"))
+        # self.GraphLabel.setStyleSheet("image: url(:/newPrefix/Graph.png);")
         self.GraphLabel.setText("")
         self.GraphLabel.setObjectName("GraphLabel")
         self.ScrollBtn = QtWidgets.QPushButton(self.centralwidget)
+        self.ScrollBtn.setIconSize(QSize(15, 15))
+        self.ScrollBtn.setIcon(QIcon("./Images/Icon awesome-greater-than.png"))
         self.ScrollBtn.setGeometry(QtCore.QRect(488, 214, 30, 30))
         self.ScrollBtn.setStyleSheet("#ScrollBtn{\n"
 "    image: url(:/newPrefix/Icon awesome-greater-than.png);\n"
@@ -178,6 +189,8 @@ class Ui_MainWindow(object):
         self.ScrollBtn.setText("")
         self.ScrollBtn.setObjectName("ScrollBtn")
         self.ScrollBtn_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.ScrollBtn_2.setIconSize(QSize(15, 15))
+        self.ScrollBtn_2.setIcon(QIcon("./Images/Icon awesome-greater-than.png"))
         self.ScrollBtn_2.setGeometry(QtCore.QRect(911, 213, 30, 30))
         self.ScrollBtn_2.setStyleSheet("#ScrollBtn_2{\n"
 "    image: url(:/newPrefix/Icon awesome-greater-than.png);\n"
@@ -243,7 +256,8 @@ class Ui_MainWindow(object):
         self.ActiveUserlabel.setObjectName("ActiveUserlabel")
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(445, 510, 18, 18))
-        self.label.setStyleSheet("image: url(:/newPrefix/Group 269.png);")
+        self.label.setPixmap(QPixmap("./Images/Group 269.png"))
+        # self.label.setStyleSheet("image: url(:/newPrefix/Group 269.png);")
         self.label.setText("")
         self.label.setObjectName("label")
         self.SeeAllBtn = QtWidgets.QPushButton(self.centralwidget)
@@ -274,22 +288,26 @@ class Ui_MainWindow(object):
         self.scrollAreaActiveUser.setWidget(self.scrollAreaWidgetContents)
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(17, 262, 54, 30))
-        self.label_2.setStyleSheet("image: url(:/newPrefix/Group 308.png);")
+        self.label_2.setPixmap(QPixmap("./Images/Group 308.png"))
+        # self.label_2.setStyleSheet("image: url(:/newPrefix/Group 308.png);")
         self.label_2.setText("")
         self.label_2.setObjectName("label_2")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(17, 330, 56, 35))
-        self.label_3.setStyleSheet("image: url(:/newPrefix/Group 309.png);")
+        self.label_3.setPixmap(QPixmap("./Images/Group 309.png"))
+        # self.label_3.setStyleSheet("image: url(:/newPrefix/Group 309.png);")
         self.label_3.setText("")
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setGeometry(QtCore.QRect(10, 403, 68, 36))
-        self.label_4.setStyleSheet("image: url(:/newPrefix/Group 310.png);")
+        self.label_4.setPixmap(QPixmap("./Images/Group 310.png"))
+        # self.label_4.setStyleSheet("image: url(:/newPrefix/Group 310.png);")
         self.label_4.setText("")
         self.label_4.setObjectName("label_4")
         self.label_5 = QtWidgets.QLabel(self.centralwidget)
         self.label_5.setGeometry(QtCore.QRect(24, 477, 40, 33))
-        self.label_5.setStyleSheet("background-image: url(:/newPrefix/Group 311.png);")
+        self.label_5.setPixmap(QPixmap("./Images/Group 311.png"))
+        # self.label_5.setStyleSheet("background-image: url(:/newPrefix/Group 311.png);")
         self.label_5.setText("")
         self.label_5.setObjectName("label_5")
         MainWindow.setCentralWidget(self.centralwidget)
