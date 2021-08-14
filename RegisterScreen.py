828f20eebbca25ebcf1c784f0703a3d83ea47204
HasnainShafiq98 <hasnainshafeeq@gmail.com>
2021-08-14 20:56:40 +0500

    Changes made

--- RegisterScreen.py
+++ RegisterScreen.py
@@ -171,6 +171,7 @@ class Ui_MainWindow_RegisterScreen(object):
                                               "\n"
                                               "}")
         self.RegisterpushButton.setObjectName("RegisterpushButton")
+        #Hasnain
         connectionString = MongoClient('mongodb://localhost:27017')
         mydb = connectionString['PSL']
         table = mydb.userRegistration
